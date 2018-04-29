0d507ee4636b24aec9a7b241c1cff2c1f784cf04
luotao <luotaoyeah@gmail.com>
2018-04-29 22:07:08 +0800

    learning node: the global object

---
+++ src/books/learning-node-2-edition/ch02/01-the-global-and-process-objects/01-the-global-object/addtow.ts
@@ -0,0 +1,5 @@
+var base: number = 2;
+
+exports.addtwo = function(input: number): number {
+  return input + base;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-import "./api/process/cwd";
+const addtwo: (
+  input: number
+) => number = require("./books/learning-node-2-edition/ch02/01-the-global-and-process-objects/01-the-global-object/addtow")
+  .addtwo;
+
+var base: number = 10;
+
+console.log(addtwo(10));
