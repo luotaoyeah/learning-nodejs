ea874e0d057371742f6c85670ad0672e21a71ba7
luotao <luotaoyeah@gmail.com>
2018-04-30 21:49:15 +0800

    learning node: Buffer.from(), Buffer.alloc(), Buffer.allocUnsafe()

--- src/books/learning-node-2-edition/ch02/02-buffer-typedarray-string/index.ts
+++ src/books/learning-node-2-edition/ch02/02-buffer-typedarray-string/index.ts
@@ -2,9 +2,6 @@
  *
  */
 
-/*
- *
- */
 console.log("\n-------------------------------------------------- 01");
 // 直接使用 new 创建一个新的 Buffer；
 const buffer01 = new Buffer(8);
@@ -13,11 +10,17 @@ buffer01.fill(0);
 /* <Buffer 00 00 00 00 00 00 00 00> */
 console.log(buffer01);
 
-/*
- *
- */
 console.log("\n-------------------------------------------------- 02");
 // 创建 Buffer 时，传入初始化的内容；
 const buffer02 = new Buffer("hello");
 /* <Buffer 68 65 6c 6c 6f> */
 console.log(buffer02);
+
+console.log("\n-------------------------------------------------- 03");
+console.log(Buffer.from([1, 2, 3]));
+/* 默认填充 0 */
+console.log(Buffer.alloc(8));
+/* 指定填充内容 */
+console.log(Buffer.alloc(8, "a"));
+/* 不会填充 */
+console.log(Buffer.allocUnsafe(8));
