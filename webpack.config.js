6c2a5b65a510a85657e0e9a62d86b73c67db03cf
luotao <luotaoyeah@gmail.com>
2018-08-18 18:27:55 +0800

    upgrade webpack@4.x

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = [
     module: {
       rules: [
         {
-          test: /\.tsx?$/,
+          test: /\.ts?$/,
           loader: "tslint-loader",
           enforce: "pre",
           include: [path.resolve("src")]
