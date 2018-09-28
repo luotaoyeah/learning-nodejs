215b39986a88e79f25955c8cbc21a5afc716785b
luotao <luotaoyeah@gmail.com>
2018-09-28 20:39:50 +0800

    mastering node: posix

---
+++ src/books/mastering-nodejs-2e/01/01-01/01-01-01/01-01-02.ts
@@ -0,0 +1,13 @@
+/*
+ * Understanding the Node Environment
+ *     Introduction – JavaScript as a systems language
+ *         POSIX
+ */
+
+/*
+ * POSIX（Portable Operating System Interface for Unix/Computer Environment）
+ * 可移植的操作系统接口；
+ * nodejs 的 fs 模块提供的 API 就是对标准 POSIX 方法的封装：
+ * The fs module provides an API for interacting with the file system in a manner closely modeled around standard POSIX functions.
+ */
+console.log("\n-------------------------------------------------- 01");
