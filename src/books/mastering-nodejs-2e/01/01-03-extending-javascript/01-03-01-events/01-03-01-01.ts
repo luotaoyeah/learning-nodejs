4614929cfd1ecf16be84f4696f6e7b7ce3ae689c
luotao <luotaoyeah@gmail.com>
2019-03-05 19:11:48 +0800

    mastering node: events: extends EventEmitter

---
+++ src/books/mastering-nodejs-2e/01/01-03-extending-javascript/01-03-01-events/01-03-01-01.ts
@@ -0,0 +1,48 @@
+/*
+ * 1. Understanding the Node Environment
+ *     1.3. Extending JavaScript
+ *         1.3.1. Events
+ */
+
+import { EventEmitter } from "events";
+
+console.log("\n-------------------------------------------------- 01");
+{
+  /*
+   * 通过继承 EventEmitter 类，可以继承 node 提供的事件相关的 API
+   */
+
+  class Counter extends EventEmitter {
+    static readonly INCREMENTED: string = "incremented";
+
+    private count: number = 0;
+
+    constructor(count?: number) {
+      super();
+      if (count) {
+        this.count = count;
+      }
+    }
+
+    increment() {
+      this.count++;
+
+      /*
+       * 通过 EventEmitter.emit() 方法，发出一个事件
+       */
+      this.emit(Counter.INCREMENTED, this.count);
+    }
+  }
+
+  const counter = new Counter(10);
+
+  /*
+   * 通过 EventEmitter.addListener() 方法，添加一个事件监听函数
+   */
+  counter.addListener(Counter.INCREMENTED, (count: number) => {
+    console.log("COUNT:", count);
+  });
+
+  counter.increment();
+  counter.increment();
+}
