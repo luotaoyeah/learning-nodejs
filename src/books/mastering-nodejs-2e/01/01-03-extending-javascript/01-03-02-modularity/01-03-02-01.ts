3b6edace1ba9952d233c6545107595a2b0599f81
luotao <luotaoyeah@gmail.com>
2019-03-06 18:42:29 +0800

    mastering node: 01-03-02: modularity

---
+++ src/books/mastering-nodejs-2e/01/01-03-extending-javascript/01-03-02-modularity/01-03-02-01.ts
@@ -0,0 +1,14 @@
+/*
+ * 1. Understanding the Node Environment
+ *     1.3. Extending JavaScript
+ *         1.3.2. Modularity
+ */
+
+console.log("\n-------------------------------------------------- 01");
+{
+  /*
+   * nodejs 使用了 CommonJS 模块系统，同时使用 npm 作为包管理器，
+   * 使用 package.json 统一管理包的信息，
+   * 包的版本使用 SemVer（Semantic Version）
+   */
+}
