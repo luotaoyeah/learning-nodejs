e8e6d4b224b872b03e6519fc9e63a5a2a08f46e3
luotao <luotaoyeah@gmail.com>
2019-03-08 20:08:47 +0800

    mastering node: 02-03-01: signals: SIGINT

---
+++ src/books/mastering-nodejs-2e/02-understanding-asynchronous-event-driven-programming/02-03-listening-for-events/02-03-01-signals/02-02-01-01.ts
@@ -0,0 +1,20 @@
+/*
+ * 2. Understanding Asynchronous Event-Driven Programming
+ *     2.3. Listening for Events
+ *         2.3.1. Signals
+ */
+
+console.log("\n-------------------------------------------------- 01");
+{
+  /*
+   * signal 指的是：操作系统给某个进程（process）发送的某个通知，
+   * 因此 node 就可以监听这些 signals 所表示的事件，
+   * 比如 SIGINT 事件表示，控制台接收到了终止程序运行的指令（CTRL + C）
+   */
+
+  setInterval(() => {}, 1e6);
+
+  process.on("SIGINT", (signal: NodeJS.Signals) => {
+    console.log("SIGINT");
+  });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-import "./books/mastering-nodejs-2e/02-understanding-asynchronous-event-driven-programming/02-02-understanding-the-event-loop/02-02-00/02-02-00-02.ts";
+import "./books/mastering-nodejs-2e/02-understanding-asynchronous-event-driven-programming/02-03-listening-for-events/02-03-01-signals/02-02-01-01.ts";
