f4f45d8b9104ff3ef2fb2d5bddef7e2669010cb5
luotao <luotaoyeah@gmail.com>
2019-03-10 13:48:05 +0800

    mastering node: 02-05-01-02: promise: onrejected and Promise.catch()

---
+++ src/books/mastering-nodejs-2e/02-understanding-asynchronous-event-driven-programming/02-05-concurrency-and-errors/02-05-01-managing-concurrency/02-05-01-02-promises/02-05-01-02-03.ts
@@ -0,0 +1,45 @@
+/*
+ * 2. Understanding Asynchronous Event-Driven Programming
+ *     2.5. Concurrency and Errors
+ *         2.5.1. Managing Concurrency
+ *             2.5.1.2. Promises
+ */
+
+console.log("\n-------------------------------------------------- 01");
+{
+  /*
+   * then() 方法的第二个参数，以及它后面的 catch() 方法，
+   * 都可以用来捕获这个 then() 方法中抛出的异常，
+   * 当 then() 方法的第二个参数（onrejected）不为空时，
+   * 则 then() 方法后面的 catch() 方法会被忽略，不会执行，
+   */
+
+  new Promise(
+    (resolve: (data?: any) => void, reject: (reason?: any) => void) => {
+      reject(new Error("ERR01"));
+    }
+  )
+    .then(
+      () => {},
+      (e: Error) => {
+        console.error(`01: ${e.message}`);
+      }
+    )
+    .catch((e: Error) => {
+      /*
+       * 这个 catch() 方法不会执行，
+       * 因为它前面的 then() 方法的第二个参数不为空
+       */
+      console.error(`02: ${e.message}`);
+    })
+    .then(() => {
+      throw new Error("ERR02");
+    })
+    .catch((e: Error) => {
+      /*
+       * 这个 catch() 方法会执行，
+       * 因为它前面的 then() 方法的第二个参数为空
+       */
+      console.error(`03: ${e.message}`);
+    });
+}
