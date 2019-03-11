cf95a4386433006722a99ab8cc8ff848490b92b8
luotao <luotaoyeah@gmail.com>
2019-03-11 18:22:13 +0800

    mastering node: 02-05-02: error: synchronous, asynchronous

---
+++ src/books/mastering-nodejs-2e/02-understanding-asynchronous-event-driven-programming/02-05-concurrency-and-errors/02-05-02-errors-and-exceptions/02-05-02-00-01.ts
@@ -0,0 +1,67 @@
+/*
+ * 2. Understanding Asynchronous Event-Driven Programming
+ *     2.5. Concurrency and Errors
+ *         2.5.2. Errors and Exceptions
+ */
+
+/*
+ * error 和 exception 的区别：
+ *     error 是非致命的（non-fatal），预料之内的（expected），
+ *     应该被捕获的（caught），应该被处理（handle）的错误，
+ * nodejs 中常见的 error 有下面四种情况：
+ */
+console.log("\n-------------------------------------------------- 01");
+{
+  /*
+   * synchronous context
+   * 比如函数参数不满足要求，直接 throw 一个 Error 对象
+   */
+
+  function fn01(x: number, y: number): number {
+    if (!Number.isInteger(x) || !Number.isInteger(y)) {
+      throw new Error("NOT INTEGER");
+    }
+
+    return x * y;
+  }
+
+  console.assert(fn01(9, 9) === 81);
+  try {
+    console.assert(fn01(9.9, 9.9) === 98.01);
+  } catch (e) {
+    console.error(`ERR01: ${e.message}`);
+  }
+}
+
+console.log("\n-------------------------------------------------- 02");
+{
+  /*
+   * asynchronous context
+   * 当调用 callback 的代码中需要抛出错误时，
+   * 应该将一个 Error 对象作为 callback 的第一个参数，
+   */
+
+  function fn01(
+    x: number,
+    y: number,
+    cb: (err: NodeJS.ErrnoException | null, data?: number) => void
+  ) {
+    if (!Number.isInteger(x) || !Number.isInteger(y)) {
+      cb(new Error("NOT INTEGER"));
+    } else {
+      cb(null, x * y);
+    }
+  }
+
+  const cb = (err: NodeJS.ErrnoException | null, data?: number) => {
+    if (err) {
+      console.error(`ERR02: ${err.message}`);
+      return;
+    }
+
+    console.log(data);
+  };
+
+  fn01(9, 9, cb);
+  fn01(9.9, 9.9, cb);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-import "./books/mastering-nodejs-2e/02-understanding-asynchronous-event-driven-programming/02-05-concurrency-and-errors/02-05-01-managing-concurrency/02-05-01-04-generator-and-iterator/02-05-01-04-02.ts";
+import "./books/mastering-nodejs-2e/02-understanding-asynchronous-event-driven-programming/02-05-concurrency-and-errors/02-05-02-errors-and-exceptions/02-05-02-00-01.ts";
