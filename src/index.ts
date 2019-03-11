d4214ebd247c61164ac2aa38642b43a973890fc7
luotao <luotaoyeah@gmail.com>
2019-03-11 19:32:27 +0800

    mastering node: 02-05-02: exception: unhandledRejection

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-import "./books/mastering-nodejs-2e/02-understanding-asynchronous-event-driven-programming/02-05-concurrency-and-errors/02-05-02-errors-and-exceptions/02-05-02-00-05.ts";
+import "./books/mastering-nodejs-2e/02-understanding-asynchronous-event-driven-programming/02-05-concurrency-and-errors/02-05-02-errors-and-exceptions/02-05-02-00-06.ts";
