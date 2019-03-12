5aa932a044673c497099a1a1fd12d304e6ecf295
luotao <luotaoyeah@gmail.com>
2019-03-12 21:35:19 +0800

    mastering node: 02-06: SSE(Server Sent Event)

--- src/books/mastering-nodejs-2e/02-understanding-asynchronous-event-driven-programming/02-06-building-a-twitter-feed-using-file-events/02-06-01.ts
+++ src/books/mastering-nodejs-2e/02-understanding-asynchronous-event-driven-programming/02-06-building-a-twitter-feed-using-file-events/02-06-01.ts
@@ -1,11 +1,42 @@
+import * as http from "http";
+import { IncomingMessage, ServerResponse } from "http";
 import { readMessage } from "./02-06-01-04";
+import * as crypto from "crypto";
 
-let response = null;
+let httpResponse: ServerResponse | null = null;
+
+http
+  .createServer((request: IncomingMessage, response: ServerResponse) => {
+    httpResponse = response;
+
+    response.writeHead(200, {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "text/event-stream"
+    });
+  })
+  .listen(8888);
 
 readMessage((e: Error | null, message?: string) => {
   if (e) {
     throw e;
   }
 
-  console.log(message);
+  if (httpResponse) {
+    httpResponse.write(`data:${message}\n\n`);
+  }
 });
+
+/*
+ * 由于在 windows 平台下，监听文件变化不起作用，
+ * 因此使用下面的代码来模拟消息来源
+ */
+setInterval(() => {
+  if (httpResponse) {
+    httpResponse.write(
+      `data:${crypto
+        .randomBytes(10)
+        .toString("hex")
+        .toUpperCase()}\n\n`
+    );
+  }
+}, 1000);
