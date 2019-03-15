d26e776bd23564bffd66b09b1accfc35ef139fd7
luotao <luotaoyeah@gmail.com>
2019-03-15 18:28:10 +0800

    mastering node: 03-02-02: Writable

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-import "./books/mastering-nodejs-2e/03-streaming-data-across-nodes-and-clients/03-02-exploring-streams/03-02-01-implementing-readable-streams/03-02-01-02.ts";
+import "./books/mastering-nodejs-2e/03-streaming-data-across-nodes-and-clients/03-02-exploring-streams/03-02-02-writable-streams/03-02-02-01.ts";
