50c6422616fbf1472675989dcd37220ca00e6350
luotao <luotaoyeah@gmail.com>
2022-06-18 14:25:07 +0800

    feat(dayjs): instanceof dayjs

--- projects/packages/dayjs/src/isDayjs.test.ts
+++ projects/packages/dayjs/src/isDayjs.test.ts
@@ -4,5 +4,8 @@ describe('src/isDayjs.test.ts', function () {
     it('01', function () {
         expect(dayjs.isDayjs(dayjs())).toEqual(true);
         expect(dayjs.isDayjs(new Date())).toEqual(false);
+
+        expect(dayjs() instanceof dayjs).toEqual(true);
+        expect(new Date() instanceof dayjs).toEqual(false);
     });
 });
